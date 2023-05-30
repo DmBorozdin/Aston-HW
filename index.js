974c61bcd2490e7e5cb63c725a5506e20bdac893
Дмитрий Бороздин <borozdin1@yandex.ru>
2023-05-30 16:31:44 +0000

    Done 1st and 2nd HW task

---
+++ index.js
@@ -0,0 +1,184 @@
+const cloneDeep = require('lodash.clonedeep');
+
+// Задание 1 – Создать объект counter всеми возможными способами;
+// Литеральная форма создания объекта counter
+{
+    let counter = {
+        count: 0,
+        increaseCount() {
+            this.count++;
+            return this.count;
+        },
+        getCount() {
+            return this.count;
+        }
+    }
+
+    console.log('Литеральная форма создания объекта:')
+    console.log(counter.getCount());
+    console.log(counter.increaseCount());
+    console.log(counter.getCount());
+}
+
+// Создание объекта с помощью функции конструктора
+{
+    function Counter() {
+        this.count = 0;
+        this.increaseCount = function() {
+            this.count++;
+            return this.count;
+        };
+        this.getCount = function() {
+            return this.count;
+        }
+    }
+
+    let counter = new Counter();
+    console.log('-------------------');
+    console.log('Создание объекта с помощью функции конструктора:');
+    console.log(counter.getCount());
+    console.log(counter.increaseCount());
+    console.log(counter.getCount());
+}
+
+// Создание объекта с помощью класса
+{
+    class Counter {
+        constructor() {
+            this.count = 0;
+        }
+
+        increaseCount() {
+            this.count++;
+            return this.count;
+        };
+
+        getCount() {
+            return this.count;
+        }
+    }
+
+    let counter = new Counter();
+    console.log('-------------------');
+    console.log('Создание объекта с помощью класса:');
+    console.log(counter.getCount());
+    console.log(counter.increaseCount());
+    console.log(counter.getCount());
+}
+
+// Создание объекта с помощью Object.create
+{
+    const counter = Object.create({},{
+        count: {
+            writable: true,
+            value: 1},
+    })
+
+    console.log('-------------------');
+    console.log('Создание объекта с помощью Object.create:')
+    console.log(counter.count);
+}
+
+
+// Создание объекта с помощью Object.assign
+{
+    let counter = Object.assign({}, {
+        count: 0,
+        increaseCount() {
+            this.count++;
+            return this.count;
+        },
+        getCount() {
+            return this.count;
+        }
+    })
+
+    console.log('-------------------');
+    console.log('Создание объекта с помощью Object.assign:')
+    console.log(counter.getCount());
+    console.log(counter.increaseCount());
+    console.log(counter.getCount());
+}
+
+
+// Задание 2 – Скопировать объект counter всеми возможными способами;
+// Копирование объекта с помощью Object.assign
+{
+    let counter = {
+        count: 0,
+        increaseCount() {
+            this.count++;
+            return this.count;
+        },
+        getCount() {
+            return this.count;
+        }
+    }
+
+    let newCounter = Object.assign({}, counter);
+    console.log('-------------------');
+    console.log('Копирование объекта с помощью Object.assign:')
+    console.log(newCounter)
+    console.log(counter === newCounter);
+}
+
+// Копирование объекта с помощью object spread()
+{
+    let counter = {
+        count: 0,
+        increaseCount() {
+            this.count++;
+            return this.count;
+        },
+        getCount() {
+            return this.count;
+        }
+    }
+
+    let newCounter = {...counter};
+    console.log('-------------------');
+    console.log('Копирование объекта с помощью object spread():')
+    console.log(newCounter);
+    console.log(counter === newCounter);
+}
+
+// Копирование объекта с помощью JSON.stringify() и JSON.parse() 
+// Методы будут опущены т.к функции не сериализуемы
+{
+    let counter = {
+        count: 1,
+        increaseCount() {
+            this.count++;
+            return this.count;
+        },
+        getCount() {
+            return this.count;
+        }
+    }
+
+    let newCounter = JSON.parse(JSON.stringify(counter));
+    console.log('-------------------');
+    console.log('Копирование объекта с помощью Object.assign:')
+    console.log(newCounter);
+    console.log(counter === newCounter);
+}
+
+// Копирование объекта с помощью lodash
+{
+    let counter = {
+        count: 0,
+        increaseCount() {
+            this.count++;
+            return this.count;
+        },
+        getCount() {
+            return this.count;
+        }
+    }
+
+    let newCounter = cloneDeep(counter);
+    console.log('-------------------');
+    console.log('Копирование объекта с помощью lodash:')
+    console.log(newCounter)
+    console.log(counter === newCounter);
+}
